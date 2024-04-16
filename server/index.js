010cb6c074e425ef262e3463ca635053f5967030
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-16 19:24:03 +0400

    add cors on server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const config = require("config");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth.routes");
 const PORT = config.get("serverPort");
+const corsMiddleware = require("./middleware/cors.middleware");
 
 const app = express();
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
