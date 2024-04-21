9cb964bb68899446339ac2ba8aa82b81eb37016f
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-21 14:05:21 +0400

    add file.service

--- server/controller/userController.js
+++
@@ -1,7 +0,0 @@
-class UserController {
-    async createUser(req, res) {
-        const { email, password } = req.body;
-    }
-}
-
-module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth.routes");
+const fileRouter = require("./routes/file.routes");
 const PORT = config.get("serverPort");
 const corsMiddleware = require("./middleware/cors.middleware");
-
 const app = express();
+
 app.use(corsMiddleware);
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/files", fileRouter);
 
 const start = async () => {
     try {
