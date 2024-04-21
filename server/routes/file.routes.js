c81e7e26de141ef0f53fd8670478cdfaeea54b11
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-21 14:32:26 +0400

    getFiles in file.router

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -4,4 +4,6 @@ const authMiddleware = require("../middleware/auth.middleware");
 const fileController = require("../controllers/file.controller");
 
 router.post("", authMiddleware, fileController.createDir);
+router.get("", authMiddleware, fileController.getFiles);
+
 module.exports = router;
