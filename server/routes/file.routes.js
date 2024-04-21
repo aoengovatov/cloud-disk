bd91055c3471836b012877bbcd6bcbfbab7bff16
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-21 14:26:13 +0400

    refactoring file.controller

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -1,5 +1,7 @@
 const Router = require("express");
 const router = new Router();
 const authMiddleware = require("../middleware/auth.middleware");
+const fileController = require("../controllers/file.controller");
 
+router.post("", authMiddleware, fileController.createDir);
 module.exports = router;
