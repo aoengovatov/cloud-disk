a0e9f43066e090d69ba040964925372a2018d43d
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-21 13:45:53 +0400

    add File model

---
+++ server/models/File.js
@@ -0,0 +1,39 @@
+const { model, Schema } = require("mongoose");
+
+const FileSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    accessLink: {
+        type: String,
+    },
+    size: {
+        type: Number,
+        default: 0,
+    },
+    path: {
+        type: String,
+        default: "",
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    parent: {
+        type: Schema.Types.ObjectId,
+        ref: "File",
+    },
+    childs: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "File",
+        },
+    ],
+});
+
+module.exports = model("File", FileSchema);
