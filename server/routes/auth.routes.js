6cd788381fc2e756685fd808f5afd82cd0308d70
Alex Engo <a.o.engovatov@yandex.ru>
2024-04-21 14:22:25 +0400

    create filePath for all users

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,8 @@ const config = require("config");
 const userMapper = require("../mappers/userMapper");
 const { check, validationResult } = require("express-validator");
 const router = new Router();
+const fileService = require("../services/file.service");
+const File = require("../models/File");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.post(
@@ -37,6 +39,7 @@ router.post(
             const hashPassword = await bcrypt.hash(password, 10);
             const user = new User({ email, password: hashPassword });
             await user.save();
+            await fileService.createDir(new File({ user: user.id, name: "" }));
 
             return res.json({ message: "User was created" });
         } catch (e) {
